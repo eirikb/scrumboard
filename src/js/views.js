cf0e1d880fa5f75ecadffb5ad93f93641d8a7936
eirikb <eirikb@eirikb.no>
2013-01-23 23:17:24 +0100

    Rotating tasks

--- src/js/views.js
+++ src/js/views.js
@@ -7,7 +7,18 @@ $(function() {
             var tpl = this.template(this.model.toJSON()).trim();
             this.setElement(tpl.trim(), true);
             this.$el.addClass('blue').draggable();
+
+            this.rotate();
             return this;
+        },
+
+        rotate: function() {
+            var deg = -4 + (Math.random() * 8);
+            var rot = 'rotate(' + deg + 'deg)';
+            this.$el.css({
+                WebkitTransform: rot,
+                MozTransform: rot
+            });
         }
     });
 
