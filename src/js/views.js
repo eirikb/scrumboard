2b702fc921f9332c6b5c40c7e437b6802372e8ef
eirikb <eirikb@eirikb.no>
2013-01-25 00:25:49 +0100

    Editable statuses, save on edit

--- src/js/views.js
+++ src/js/views.js
@@ -68,6 +68,22 @@ $(function() {
     app.StatusView = Backbone.View.extend({
         template: _.template($('#status-template').html()),
 
+        events: {
+            'dblclick h2': 'edit',
+            'blur h2': 'doneedit'
+        },
+
+        edit: function(e) {
+            $(e.target).attr('contenteditable', true).focus();
+        },
+
+        doneedit: function(e) {
+            var title = this.$('h2').text().trim();
+            this.model.save({
+                title: title
+            });
+        },
+
         render: function() {
             var tpl = this.template(this.model.toJSON()).trim();
             this.setElement(tpl.trim(), true);
