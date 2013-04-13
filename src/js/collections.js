73062b19e8bb8851e79ee8dab8b8618649038abc
Eirik Brandtzaeg <eirikb@eirikb.no>
2013-04-13 08:57:52 +0200

    localstorage is now optional. Url (when no localstorage) can be passed through config.*Url

--- src/js/collections.js
+++ src/js/collections.js
@@ -2,16 +2,19 @@ $(function() {
 
     app.TaskList = Backbone.Collection.extend({
         model: app.Task,
-        localStorage: new Backbone.LocalStorage('Tasks')
+        url: window.config && config.taskUrl,
+        localStorage: Backbone.LocalStorage && new Backbone.LocalStorage('Tasks')
     });
 
     app.StatusList = Backbone.Collection.extend({
         model: app.Status,
-        localStorage: new Backbone.LocalStorage('Statuses')
+        url: window.config && config.statusUrl,
+        localStorage: Backbone.LocalStorage && new Backbone.LocalStorage('Statuses')
     });
 
     app.UserList = Backbone.Collection.extend({
         model: app.User,
-        localStorage: new Backbone.LocalStorage('Users')
+        url: window.config && config.userUrl,
+        localStorage: Backbone.LocalStorage && new Backbone.LocalStorage('Users')
     });
 });
